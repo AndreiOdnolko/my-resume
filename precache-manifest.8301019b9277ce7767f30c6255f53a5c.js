b5a1007f1d6dd97cd4462a345c4c0e00126a317c
Andrew <odnolko.andrej@yandex.ru>
2018-11-24 23:54:33 +0300

    Updates

--- precache-manifest.8301019b9277ce7767f30c6255f53a5c.js
+++ precache-manifest.8301019b9277ce7767f30c6255f53a5c.js
@@ -4,12 +4,12 @@ self.__precacheManifest = [
     "url": "/my-resume/static/media/photo.74be3c0c.jpg"
   },
   {
-    "revision": "ce9ba54c4eb7f4b1b40c",
-    "url": "/my-resume/static/css/main.844567e8.chunk.css"
+    "revision": "c72304c2143b82b18b7e",
+    "url": "/my-resume/static/css/main.e67ba1f2.chunk.css"
   },
   {
-    "revision": "8028a11927f48f1d039e",
-    "url": "/my-resume/static/js/1.8028a119.chunk.js"
+    "revision": "15529b53ed2290741110",
+    "url": "/my-resume/static/js/1.15529b53.chunk.js"
   },
   {
     "revision": "54fe26f0c54799edc228",
@@ -24,12 +24,16 @@ self.__precacheManifest = [
     "url": "/my-resume/static/media/russia.c6c045c4.png"
   },
   {
-    "revision": "ce9ba54c4eb7f4b1b40c",
-    "url": "/my-resume/static/js/main.ce9ba54c.chunk.js"
+    "revision": "c72304c2143b82b18b7e",
+    "url": "/my-resume/static/js/main.c72304c2.chunk.js"
   },
   {
     "revision": "ccebbd0a348bae2d9cb6e673bf533463",
     "url": "/my-resume/static/media/todolist.ccebbd0a.png"
+  },
+  {
+    "revision": "7f88740f7211341d98611897e93dc9c9",
+    "url": "/my-resume/static/media/first.7f88740f.png"
   },
   {
     "revision": "7e7f6dd5310e4759228286c280dd01d3",
@@ -40,11 +44,7 @@ self.__precacheManifest = [
     "url": "/my-resume/static/media/fruitBasket.d3b1b313.png"
   },
   {
-    "revision": "7f88740f7211341d98611897e93dc9c9",
-    "url": "/my-resume/static/media/first.7f88740f.png"
-  },
-  {
-    "revision": "4432251a0867657f0c612fa4cfee3caf",
+    "revision": "8d4cdac607014e74963f7063ca48269c",
     "url": "/my-resume/index.html"
   }
 ];
